b1dd1a490987658a2452fa73848c5f02148840de
jos3duardo <jos3duardolopes@gmail.com>
2020-05-08 21:50:51 -0400

    footer component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <div>
-    <nav-bar cor="green darken-1" url="#" logo="Social" />
-    <router-view/>
-  </div>
+  <span>
+    <header>
+      <nav-bar-vue cor="green darken-1" url="#" logo="Social" />
+    </header>
+    <main>
+      <div class="container">
+        <router-view/>
+      </div>
+    </main>
+    <footer-vue cor="green darken-1" logo="Social" descricao="Teste de descrição" ano="2020">
+        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+    </footer-vue>
+  </span>
 </template>
 
 <script>
-import NavBar from "./components/layouts/NavBar";
+import NavBarVue from "./components/layouts/NavBarVue";
+import FooterVue from "./components/layouts/FooterVue";
 
 export default {
   name: 'App',
-  components: {NavBar}
+  components: {
+    NavBarVue,
+    FooterVue
+  }
 }
 </script>
 
---
+++ src/components/layouts/FooterVue.vue
@@ -0,0 +1,43 @@
+<template>
+    <footer :class=" 'page-footer ' + cor || 'red'">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">{{logo || 'Site'}}</h5>
+            <p class="grey-text text-lighten-4">{{ descricao || 'Sua descrição.' }}</p>
+          </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+              <slot>
+
+              </slot>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+          © {{ ano || '2020' }} Copyright {{ logo || 'Site' }}
+          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script>
+  export default {
+    name: 'FooterVue',
+    props: ['cor','logo', 'descricao','ano'],
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
