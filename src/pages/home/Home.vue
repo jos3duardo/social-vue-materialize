76fe37f7c5a6c1c15e6e3fca2c314acb7ed807e2
jos3duardo <jos3duardolopes@gmail.com>
2020-05-08 23:21:10 -0400

    create component cardConteudo

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,28 +1,42 @@
 <template>
-  <card-conteudo-vue
-    perfil="https://materializecss.com/images/yuna.jpg"
-    nome="Loana Sousa"
-    data="08/05/2020 22:32">
-    <card-detalhe-vue
-      imagem="https://materializecss.com/images/sample-1.jpg"
-      titulo=""
-      texto="I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively."
-    >
+  <span>
 
-    </card-detalhe-vue>
-  </card-conteudo-vue>
+    <div class="row">
+        <grid-vue class="input-field" tamanho="12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Oo que está acontecendo?</label>
+        </grid-vue>
+        <p>
+          <button class="btn waves-effect waves-light s2 offset-s10 right">Publicar</button>
+        </p>
+    </div>
+
+    <card-conteudo-vue
+      perfil="https://materializecss.com/images/yuna.jpg"
+      nome="Loana Sousa"
+      data="08/05/2020 22:32">
+        <card-detalhe-vue
+          imagem="https://materializecss.com/images/sample-1.jpg"
+          titulo=""
+          texto="I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively."
+        />
+    </card-conteudo-vue>
+  </span>
+
 </template>
 
 <script>
   import CardConteudoVue from "../../components/social/CardConteudoVue";
   import CardDetalheVue from "../../components/social/CardDetalheVue";
+  import GridVue from "../../components/layouts/GridVue";
 
   export default {
     name: 'Home',
     components: {
       CardConteudoVue,
-      CardDetalheVue
+      CardDetalheVue,
+      GridVue
     },
     data () {
       return {
