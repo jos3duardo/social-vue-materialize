f8a5a7942064f02b2b0a6cefb361eae7e3185031
jos3duardo <jos3duardolopes@gmail.com>
2020-05-08 22:00:30 -0400

    grid component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,16 @@
     </header>
     <main>
       <div class="container">
-        <router-view/>
+        <div class="row">
+
+          <grid-vue tamanho="4">
+            Menu
+          </grid-vue>
+          <grid-vue tamanho="8">
+            <router-view/>
+          </grid-vue>
+
+        </div>
       </div>
     </main>
     <footer-vue cor="green darken-1" logo="Social" descricao="Teste de descrição" ano="2020">
@@ -20,12 +29,14 @@
 <script>
 import NavBarVue from "./components/layouts/NavBarVue";
 import FooterVue from "./components/layouts/FooterVue";
+import GridVue from "./components/layouts/GridVue";
 
 export default {
   name: 'App',
   components: {
     NavBarVue,
-    FooterVue
+    FooterVue,
+    GridVue
   }
 }
 </script>
