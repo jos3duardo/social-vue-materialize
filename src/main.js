7c6adce9f7830ac7da8baa1decb34896bd2b4eff
jos3duardo <jos3duardolopes@gmail.com>
2020-05-12 20:01:34 -0400

    add vuex in app

--- src/main.js
+++ src/main.js
@@ -4,14 +4,33 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import Vuex from 'vuex'
+
+Vue;use(Vuex)
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.prototype.$urlAPI = 'http://127.0.0.1:8000/api/'
 
+var store = {
+  state: {
+    name: 'José Eduardo'
+  },
+  getters:{
+    getNome: state => {
+      return state.name
+    }
+  },
+  mutations:{
+
+  }
+}
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: new Vuex.Store(store),
   router,
   components: { App },
   template: '<App/>'
